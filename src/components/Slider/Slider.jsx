00b605f37c2dfb4958b55645bc313e0e2f2bd081
GabrielZolk <dialixkat@gmail.com>
2023-07-02 19:13:25 -0300

    feat: slider change

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,24 +1,89 @@
+import { useState, useEffect } from "react";
 import { Container, DefaultSlider, SliderContent } from "./Slider.style";
 
-
 export default function Slider() {
-    return (
-        <Container>
-            <DefaultSlider>
-                <h3>Pigz Marketplace</h3>
-                <SliderContent>
-                    <p>
-                        Além de uma página exclusiva, com o Pigz Marketplace sua loja terá mais visibilidade, no app e no site. Uma vitrine com milhares de clientes todos os dias, pra vender muito mais.
-                    </p>
-                    <div>
-                        <img src="phones.png" alt="two cell phones" />
-                    </div>
-                </SliderContent>
-            </DefaultSlider>
-            <div className="support-info">
-                <h4>E mais: suporte que realmente funciona!</h4>
-                <p>Respostas automáticas e robôs? Aqui não. Nossa equipe está sempre disponível pra ajudar você e seus clientes.</p>
+  const [activeSlide, setActiveSlide] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setActiveSlide((prevSlide) => (prevSlide + 1) % 4);
+    }, 10000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+
+  const handleSlideChange = (index) => {
+    setActiveSlide(index);
+  };
+
+  return (
+    <Container>
+      <div className="slider-navigation">
+        {[0, 1, 2, 3].map((index) => (
+          <div
+            key={index}
+            className={`slider-bar ${activeSlide === index ? 'active' : ''}`}
+            onClick={() => handleSlideChange(index)}
+          ></div>
+        ))}
+      </div>
+
+      <div className="slider-wrapper">
+        <DefaultSlider className={`slider ${activeSlide === 0 ? 'active' : ''}`}>
+          <h3>Pigz Marketplace</h3>
+          <SliderContent>
+            <p>
+              Além de uma página exclusiva, com o Pigz Marketplace sua loja terá mais visibilidade, no app e no site. Uma vitrine com milhares de clientes todos os dias, pra vender muito mais.
+            </p>
+            <div>
+              <img src="phones.png" alt="two cell phones" />
             </div>
-        </Container>
-    )
+          </SliderContent>
+        </DefaultSlider>
+
+        <DefaultSlider className={`slider ${activeSlide === 1 ? 'active' : ''}`}>
+          <h3>Pigz Gestão</h3>
+          <SliderContent>
+            <p>
+              Acompanhe suas vendas em tempo real, no computador ou no celular, de onde estiver. Faça alterações de preços e disponibilidade de produtos rapidamente, como deve ser.
+            </p>
+            <div>
+              <img src="notebook.png" alt="two cell phones" />
+            </div>
+          </SliderContent>
+        </DefaultSlider>
+
+        <DefaultSlider className={`slider ${activeSlide === 2 ? 'active' : ''}`}>
+          <h3>Gestão de entregadores</h3>
+          <SliderContent>
+            <p>
+              A ferramenta ideal pra quem tem entrega própria. Controle de entregas por motoboy, geração de relatórios por entregas e por taxas de entrega, individualmente.
+            </p>
+            <div>
+              <img src="mobile.png" alt="two cell phones" />
+            </div>
+          </SliderContent>
+        </DefaultSlider>
+
+        <DefaultSlider className={`slider ${activeSlide === 3 ? 'active' : ''}`}>
+          <h3>Pagamento on-line</h3>
+          <SliderContent>
+            <p>
+              Segurança e agilidade para receber e entregar pedidos. Seus clientes pagam com Pix ou cartão de crédito pelo app, e o entregador nem precisa levar a maquininha de cartão.
+            </p>
+            <div>
+              <img src="card.png" alt="two cell phones" />
+            </div>
+          </SliderContent>
+        </DefaultSlider>
+      </div>
+
+      <div className="support-info">
+        <h4>E mais: suporte que realmente funciona!</h4>
+        <p>Respostas automáticas e robôs? Aqui não. Nossa equipe está sempre disponível pra ajudar você e seus clientes.</p>
+      </div>
+    </Container>
+  );
 }
--- src/components/Slider/Slider.style.jsx
+++ src/components/Slider/Slider.style.jsx
@@ -1,65 +1,108 @@
 import { styled } from "styled-components";
 
 export const Container = styled.div`
-    h3 {
-        color: #333333;
-        text-align: center;
-        margin-bottom: 40px;
-        font-size: 24px;
-        font-family: 'Poppins', sans-serif;
-        font-weight: 600;
+  h3 {
+    color: #333333;
+    text-align: center;
+    margin-bottom: 40px;
+    font-size: 24px;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 600;
+  }
+
+  .slider-navigation {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+
+  .slider-bar {
+    width: 88px;
+    height: 4px;
+    margin: 0 5px;
+    background-color: #EEEEEE;
+    border-radius: 4px;
+    cursor: pointer;
+  }
+
+  .slider-bar.active {
+    background-color: #FA641E;
+  }
+
+  .slider-wrapper {
+    position: relative;
+    height: 500px;
+    overflow: hidden;
+  }
+
+  .slider {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    transition: opacity 0.3s ease-in-out;
+    pointer-events: none;
+  }
+
+  .slider.active {
+    opacity: 1;
+    pointer-events: initial;
+  }
+
+  .support-info {
+    padding: 0 40px;
+
+    h4 {
+      margin: 50px auto 0;
+      text-align: center;
+      color: #333333;
+      max-width: 300px;
+      line-height: 38px;
+      font-size: 30px;
+      font-family: 'Poppins', sans-serif;
+      font-weight: 700;
     }
 
-    .support-info {
-        padding: 0 40px;
-            h4 {
-                margin: 80px auto 0;
-                text-align: center;
-                color: #333333;
-                max-width: 300px;
-                line-height: 38px;
-                font-size: 30px;
-                font-family: 'Poppins', sans-serif;
-                font-weight: 700;
-        }
-
-        p {
-            padding: 0 20px;
-            margin: 20px 0 40px 0;
-            text-align: center;
-            max-width: 330px;
-            line-height: 23px;
-            color: #333333;
-            font-size: 16px;
-            font-family: 'Poppins', sans-serif;
-            font-weight: 500;
-        }
+    p {
+      padding: 0 20px;
+      margin: 20px 0 10px 0;
+      text-align: center;
+      max-width: 330px;
+      line-height: 23px;
+      color: #333333;
+      font-size: 16px;
+      font-family: 'Poppins', sans-serif;
+      font-weight: 500;
     }
+  }
 `;
 
 export const DefaultSlider = styled.div`
-       padding: 0 16px;
+  padding: 0 16px;
 `;
 
 export const SliderContent = styled.div`
-    background: linear-gradient(to top, #FF881F, #FA641E);
-    border-radius: 70px;
+  background: linear-gradient(to top, #FF881F, #FA641E);
+  border-radius: 70px;
 
-    p {
-        padding: 40px 40px 50px 40px;
-        color: #FFFFFF;
-        text-align: center;
-        font-size: 15px;
-        font-family: 'Poppins', sans-serif;
-        font-weight: 200;
-        line-height: 20px;
-    }
+  p {
+    padding: 40px 40px 50px 40px;
+    color: #FFFFFF;
+    text-align: center;
+    font-size: 15px;
+    font-family: 'Poppins', sans-serif;
+    font-weight: 200;
+    line-height: 20px;
+  }
 
-    div {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin: 0 auto; 
-        overflow: hidden; 
-    }
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto; 
+    overflow: hidden;
+  }
 `;
